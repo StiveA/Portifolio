a02f4dd60ff0c521f5bb2b2f82eb2152463f131b
StiveA <stiveatormes@gmail.com>
2021-10-25 16:04:04 -0300

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React from "react"
+import scrollTo from "gatsby-plugin-smoothscroll"
+const Navbar = () => {
+  return (
+    <div className="section">
+      <div className="container">
+        <div className="navbar-wrapper">
+          <div
+            role="button"
+            onClick={() => scrollTo("#home")}
+            className="name"
+            tabIndex={0}
+          >
+            Portfolio.
+          </div>
+          <div className="links-wrapper">
+            <button onClick={() => scrollTo("#work")}>Trabalho</button>
+            <button onClick={() => scrollTo("#about")}>Sobre</button>
+            <button onClick={() => scrollTo("#contact")}>Contato</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
